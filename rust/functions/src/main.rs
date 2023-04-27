90c641dffadee71071fabe3aeb48b04760eb7775
Hyeonsu <hyeonsuid@gmail.com>
2023-04-28 02:20:13 +0900

    made branches project

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     println!("Hello, world!");
     another_function(5);
     print_labeled_measurement(5, 'h');
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x: i32) {
@@ -10,4 +13,8 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
